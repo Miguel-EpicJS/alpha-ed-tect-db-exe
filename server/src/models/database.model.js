b4555fd6c4f8c32d577f8e745a603f9c72782bad
CodigoDeSenior <migueljnm840@gmail.com>
2021-11-16 15:53:07 -0300

    get and add posts db models

--- server/src/models/database.model.js
+++ server/src/models/database.model.js
@@ -59,7 +59,32 @@ module.exports = {
                 const sql = `INSERT INTO "public"."public.ae_Category" ("Name", "Description", "Deleted") VALUES ($1, $2, $3)`;
                 const result = client.query(sql, [category.name, category.description, false]);
                 return result;
-
+            }
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    getPosts: (limit) => {
+        try {
+            if (limit > 0 && limit < 100000) {
+                const sql = `SELECT * FROM "public"."public.ae_Posts" LIMIT LIMIT $1`;
+                const result = client.query(sql, [limit]);
+                return result;
+            } else {
+                new Error("The value is bellow 0, or is above 100000 or isn't a number");
+            }
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
+    },
+    addPost: (post) => {
+        try {
+            if(category) {
+                const sql = `INSERT INTO "public"."public.ae_Posts" ("Title", "Subtitle", "Content", "About", "Image_link", "Posted_by", "Validated", "Validated_by", "Category", "Delted") VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
+                const result = client.query(sql, [post.title, post.subtitle, post.content, post.about, post.image_link, post.posted_by, post.validated, post.validated_by, post.category, false]);
+                return result;
             }
         } catch (error) {
             console.log(error);
