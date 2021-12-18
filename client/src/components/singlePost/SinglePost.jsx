a18195beff0d3ad578a5839b32fec58a9d6db64b
CodigoDeSenior <migueljnm840@gmail.com>
2021-12-18 11:14:09 -0300

    Simple like button

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 import { useEffect, useState } from "react";
+import { AiFillLike } from "react-icons/ai"
 
 import { Link } from "react-router-dom";
 import { useParams } from 'react-router-dom';
@@ -12,11 +13,17 @@ export default function SinglePost() {
   const { postId } = useParams();
 
   const [post, setPost] = useState({});
+  const [liked, setLiked] = useState(false);
   
   useEffect(() => {
     axios.get(`http://127.0.0.1:4000/post/show-post/${postId}`).then(res => setPost(res.data));
     console.log(post);
   },[]);
+
+  const likeButton = () => {
+    setLiked(!liked);
+    console.log(liked);
+  }
 
   return (
     <div className="singlePost">
@@ -39,6 +46,10 @@ export default function SinglePost() {
             <Link className="link" to={`/?user=${post.username}`}>
               Category: <b>{post.cat}</b>
             </Link>
+            |
+            <button className="link icon" onClick={() => { likeButton()}}>
+              <b className={`${liked === true ? "liked" : ""}`}><AiFillLike /></b>
+            </button>
           </div>
         </div>
         <div className="singlePostText">
